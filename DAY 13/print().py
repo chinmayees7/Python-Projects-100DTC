4ee862926da44ec1d85e3a13088148f66a8536a6
chinmayees7 <chinpsaw@fmail.com>
2025-02-03 18:35:03 +0530

    Day 13

---
+++ DAY 13/print().py
@@ -0,0 +1,15 @@
+word_per_page = 0
+
+pages = int(input("Number of pages:"))
+
+word_per_page == int(input("Number of words per page:"))
+print(f"Pages = {pages} Words per page = {word_per_page}") 
+total_words = pages * word_per_page
+print(total_words)
+
+# '==" is used to check if the values are equal and return a boolean but since we want to assign the value, '=' must be used."
+
+word_per_page = int(input("Number of words per page:"))
+print(f"Pages = {pages} Words per page = {word_per_page}") 
+total_words = pages * word_per_page
+print(total_words)
